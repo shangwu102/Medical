ee92bc78bf45895e83b1fdb75070c82aa9c6219b
wcq <1514320887@qq.com>
2024-03-11 20:11:15 +0800

    初始化

---
+++ src/main/java/com/shangwu/MedicalWebApplication.java
@@ -0,0 +1,13 @@
+package com.shangwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalWebApplication.class, args);
+    }
+
+}
